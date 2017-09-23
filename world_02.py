d8406b6b108bafb83faa33ab60b803cc0b515def
Tom Young <tyoung@n.io>
2017-09-23 10:28:11 -0600

    significant refactor

---
+++ world_02.py
@@ -0,0 +1,67 @@
+import math
+import random
+import pygame
+
+
+class World():
+
+    PLAYER_RADIUS = 5
+    TURN_RADIUS = 10
+    STEP = 1
+    START_FOODS = 50
+    WORLD_SIZE = (800, 800)
+    
+    _WIDTH, _HEIGHT = WORLD_SIZE
+    _TURN = 360 / ((TURN_RADIUS * math.pi) * STEP)
+
+    def __init__(self, SEED=None, render=False):
+        if SEED:
+            random.seed(int(SEED))
+        if render:
+            pygame.init()
+            pygame.font.init()
+            surface = pygame.display.set_mode(self.WORLD_SIZE)
+            pygame.display.set_caption('window title goes here')
+            default_font = pygame.font.Font(None, 30)
+        self.render = render
+        self.foods = []
+        for _ in range(self.START_FOODS):
+            self.foods.append(self._random_coords())
+        self.player_x, self.player_y = self._random_coords()
+        self.player_heading = random.randint(0, 359)
+
+    def frame(self, input):
+        self.player_heading = (self.player_heading + input * self._TURN) % 360
+        heading_rads = math.radians(self.player_heading)
+        self.player_x += (math.sin(heading_rads) * self.STEP) % self._WIDTH
+        self.player_y += (math.cos(heading_rads) * self.STEP) % self._HEIGHT
+        food_vectors = []
+        for x, y in self.foods:
+            vector = (
+                abs(self.player_x - x),
+                abs(self.player_y - y))
+            food_vectors.append(vector)
+        t_food_vectors = []
+        for x, y in food_vectors:
+            vector = (
+                min(vector[0], self._WIDTH - x),
+                min(vector[1], self._HEIGHT - y))
+            t_food_vectors.append(vector)
+        food_distances = [self._distance(vector) for vector in t_food_vectors]
+        remaining_foods = []
+        for index, distance in enumerate(food_distances):
+            if distance > self.PLAYER_RADIUS:
+                remaining_foods.append(self.foods[index])
+        if not remaining_foods:
+            return None
+        self.foods = remaining_foods
+        return min(food_distances)
+
+    def _random_coords(self):
+        return (
+            random.randint(0, self._WIDTH - 1),
+            random.randint(0, self._HEIGHT - 1))
+
+    @staticmethod
+    def _distance(vector):
+        return math.sqrt(sum([v**2 for v in vector]))

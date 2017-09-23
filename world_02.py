ad37f840cc1e65fd968a0ae16e2c2a3b6522b149
Tom Young <tyoung@n.io>
2017-09-23 10:56:41 -0600

    moar werdz

--- world_02.py
+++ world_02.py
@@ -10,20 +10,20 @@ class World():
     STEP = 1
     START_FOODS = 50
     WORLD_SIZE = (800, 800)
+    RENDER = False
 
     _WIDTH, _HEIGHT = WORLD_SIZE
     _TURN = 360 / ((TURN_RADIUS * math.pi) * STEP)
 
-    def __init__(self, SEED=None, RENDER=False):
+    def __init__(self, SEED=None):
         if SEED:
             random.seed(int(SEED))
-        if RENDER:
+        if self.RENDER:
             pygame.init()
             pygame.font.init()
             surface = pygame.display.set_mode(self.WORLD_SIZE)
             pygame.display.set_caption('window title goes here')
             default_font = pygame.font.Font(None, 30)
-        self.render = RENDER
         self.foods = []
         for _ in range(self.START_FOODS):
             self.foods.append(self._random_coords())
@@ -33,21 +33,17 @@ class World():
     def frame(self, input):
         self.player_heading = (self.player_heading + input * self._TURN) % 360
         heading_rads = math.radians(self.player_heading)
-        self.player_x += (math.sin(heading_rads) * self.STEP) % self._WIDTH
-        self.player_y += (math.cos(heading_rads) * self.STEP) % self._HEIGHT
+        self.player_x = (
+            self.player_x + math.sin(heading_rads) * self.STEP) % self._WIDTH
+        self.player_y = (
+            self.player_y - math.cos(heading_rads) * self.STEP) % self._HEIGHT
         food_vectors = []
         for x, y in self.foods:
             vector = (
-                abs(self.player_x - x),
-                abs(self.player_y - y))
+                min(abs(self.player_x - x), self._WIDTH - x),
+                min(abs(self.player_y - y), self._HEIGHT - y))
             food_vectors.append(vector)
-        t_food_vectors = []
-        for x, y in food_vectors:
-            vector = (
-                min(vector[0], self._WIDTH - x),
-                min(vector[1], self._HEIGHT - y))
-            t_food_vectors.append(vector)
-        food_distances = [self._distance(vector) for vector in t_food_vectors]
+        food_distances = [self._distance(vector) for vector in food_vectors]
         remaining_foods = []
         for index, distance in enumerate(food_distances):
             if distance > self.PLAYER_RADIUS:

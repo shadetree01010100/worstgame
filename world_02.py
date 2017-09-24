029b3e4b301d0b32cc814862d9c9c4d8726cb68c
Tom Young <tyoung@n.io>
2017-09-23 22:16:58 -0600

    enhance the world

--- world_02.py
+++ world_02.py
@@ -9,19 +9,20 @@ class World():
     WIDTH = 800
     HEIGHT = 800
     RENDER = False
+    EPISODE_LIMIT = 100000
 
     PLAYER_RADIUS = 5
-    TURN_DEG = 5
+    TURN_DEG = 30
     STEP = 1
 
-    def __init__(self, SEED=None):
+    def __init__(self, WINDOW_TITLE, SEED=None):
         if SEED:
             random.seed(int(SEED))
         if self.RENDER:
             pygame.init()
             pygame.font.init()
             self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-            pygame.display.set_caption('window title goes here')
+            pygame.display.set_caption(WINDOW_TITLE)
             self.font = pygame.font.Font(None, 30)
             self.clock = pygame.time.Clock()
         self.episodes = 0
@@ -30,6 +31,8 @@ class World():
             self.foods.append(self._random_coords())
         self.player_x, self.player_y = self._random_coords()
         self.player_heading = random.randint(0, 359)
+        # todo: return nearest
+        self.nearest_food = None
 
     def episode(self, input):
         self.episodes += 1
@@ -44,47 +47,56 @@ class World():
         remaining_foods = []
         food_distances = []
         for x, y in self.foods:
+            delta_x = abs(self.player_x - x)
+            delta_y = abs(self.player_y - y)
             vector = (
-                min(abs(self.player_x - x), self.WIDTH - x),
-                min(abs(self.player_y - y), self.HEIGHT - y))
+                min(delta_x, self.WIDTH / 2 - abs(self.WIDTH / 2 - delta_x)),
+                min(delta_y, self.HEIGHT / 2 - abs(self.HEIGHT / 2 - delta_y)))
             distance = self._distance(vector)
             if distance > self.PLAYER_RADIUS:
                 food_vectors.append(vector)
                 remaining_foods.append((x, y))
                 food_distances.append(distance)
-        if not remaining_foods:
+        if not remaining_foods or self.episodes == self.EPISODE_LIMIT:
             if self.RENDER:
-                pygame.quit()
+                pygame.display.quit()
             return None
         self.foods = remaining_foods
-        if self.RENDER:
+        self.nearest_food = min(food_distances)
+        if self.RENDER and self.episodes % self.RENDER == 0:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
+                    pygame.display.quit()
                     return None
             self.surface.fill((0, 0, 0))
-            self.draw_player()
             for food in self.foods:
-                self.draw_food(food)
-            self.draw_stats()
+                self._draw_food(food)
+            self._draw_player()
+            self._draw_stats(input)
             pygame.display.update()
-            self.clock.tick(0)
-        return min(food_distances)
+            self.clock.tick()
+        return self.nearest_food
 
-    def draw_player(self, color=(255, 255, 255)):
+    def _draw_player(self, color=(255, 255, 255)):
         position = (int(round(self.player_x)), int(round(self.player_y)))
         pygame.draw.circle(self.surface, color, position, self.PLAYER_RADIUS)
 
-    def draw_food(self, position, color=(0, 255, 0)):
+    def _draw_food(self, position, color=(0, 255, 0)):
         x, y = position
         pygame.draw.line(self.surface, color, (x, y - 2), (x, y + 2))
-        pygame.draw.line(self.surface, color, (x - 2, y), (x + 2, y))
+        pygame.draw.line(self.surface, color, (x - 2, y ), (x + 2, y))
 
-    def draw_stats(self, color=(255, 255, 255)):
-        elapsed_ep = self.font.render(str(self.episodes), False, color)
-        remaining = self.font.render(str(len(self.foods)), False, color)
+    def _draw_stats(self, input, color=(255, 255, 255)):
+        elapsed_ep = self.font.render('E: {}'.format(self.episodes), False, color)
+        remaining = self.font.render('R: {}'.format(len(self.foods)), False, color)
+        episode_in = self.font.render(
+            'I: {}'.format(round(input, 2)), False, color)
+        episode_out = self.font.render(
+            'O: {}'.format(round(self.nearest_food, 1)), False, color)
         self.surface.blit(elapsed_ep, (1, 0))
         self.surface.blit(remaining, (1, 16))
+        self.surface.blit(episode_in, (1, 32))
+        self.surface.blit(episode_out, (1, 48))
 
     def _random_coords(self):
         return (

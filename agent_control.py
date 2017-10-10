bb37e20060a63c4aeed15eb398aa70f5086721cb
Tom Young <tyoung@n.io>
2017-10-09 19:24:09 -0600

    add random choice agent

---
+++ agent_control.py
@@ -0,0 +1,24 @@
+import random
+from world_02 import World
+
+
+RENDER = 1
+trials = 1
+results = []
+for trial in range(trials):
+    WINDOW_TITLE = 'trial {} of {}'.format(trial + 1, trials)
+    w = World(WINDOW_TITLE, RENDER=RENDER)
+    i = 1
+    while not w.done():
+        move = random.choice([-1, 0, 1])
+        w.episode(move)
+        i += 1
+    if i < w.EPISODE_LIMIT:
+        results.append(i)
+if results:
+    average = int(round(sum(results) / len(results)))
+    print('\t{} average performance, excluding {} failures'.format(
+        average,
+        trials - len(results)))
+else:
+    print('\t... literally can\'t even ({} trials)'.format(trials))

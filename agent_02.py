af969a2b79a952c4145e388b7004bdfac6999cbb
Tom Young <tyoung@n.io>
2017-10-09 19:14:39 -0600

    this may actually perform worse than random chance

---
+++ agent_02.py
@@ -0,0 +1,27 @@
+import random
+from world_02 import World
+
+
+RENDER = 1
+trials = 1
+results = []
+for trial in range(trials):
+    WINDOW_TITLE = 'trial {} of {}'.format(trial + 1, trials)
+    w = World(WINDOW_TITLE, RENDER=RENDER)
+    move = random.choice([-1, 1])
+    last_dist = w.player.closest_food()
+    w.episode(move)
+    dist = w.player.closest_food()
+    i = 1
+    while not w.done():
+        if dist > last_dist:
+            move *= -1
+        last_dist = dist
+        w.episode(move)
+        dist = w.player.closest_food()
+        i += 1
+    if i < w.EPISODE_LIMIT:
+        results.append(i)
+print('\t{} average performance, excluding {} failures'.format(
+    int(round(sum(results) / len(results))),
+    trials - len(results)))
